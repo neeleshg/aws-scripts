aa64f3992227cb205d1b696f669131fcf55d2ea7
Neelesh <neelesh@pulseid.com>
2019-12-23 05:44:49 +0530

    Added multi profile and multi region support with CSV writing

--- get_instance_details_profiles.py
+++ get_instance_details_profiles.py
@@ -6,12 +6,9 @@ Usage:
    - export AWS_CONFIG_FILE=~/.aws/config
    - export AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials
    - python get_instance_details_profiles.py <Profile Name> <Regions separated by Commas>
-     For eg. python get_instance_details_profiles.py test us-east-1,us-west-1
+	 For eg. python get_instance_details_profiles.py test us-east-1,us-west-1
 
-   OR to check in all region dont give any regions
-     For eg. python get_instance_details_profiles.py test
-
-   
+If you dont give any region, it will give an error.
 
   Example of AWS_CONFIG file:
 
@@ -36,33 +33,36 @@ import boto3
 import os
 import sys
 from datetime import datetime
+import csv
 
-profile_name = sys.argv[1]
+
 
 aws_regions = None
 
 if len(sys.argv) == 3:
-	aws_regions = list(sys.argv[2].split(','))  ### Add regions in this list
-
-session = boto3.Session(profile_name=profile_name)
-
+	aws_regions   = list(sys.argv[2].split(','))  ### Add regions in this list
+	profile_names = list(sys.argv[1].split(','))  ## Add Profiles in the list
 
 # Function to get Instace Count per type
 def get_instance_count_per_type(session,aws_region):
-    ec2_client = session.client('ec2',region_name=aws_region)
-    instance_type = {}
-    response = ec2_client.describe_instances()
-    for i in response['Reservations']:
-    	if i['Instances'][0]['InstanceType'] in instance_type:
-    		instance_type[i['Instances'][0]['InstanceType']] = instance_type[i['Instances'][0]['InstanceType']] + 1
-    	else:
-    		instance_type[i['Instances'][0]['InstanceType']] = 1
-    return(instance_type)
+	print("Getting Instance Details")
+	ec2_client = session.client('ec2',region_name=aws_region)
+	instance_type = {}
+	response = ec2_client.describe_instances()
+	for i in response['Reservations']:
+		if i['Instances'][0]['InstanceType'] in instance_type:
+			instance_type[i['Instances'][0]['InstanceType']] = instance_type[i['Instances'][0]['InstanceType']] + 1
+		else:
+			instance_type[i['Instances'][0]['InstanceType']] = 1
+
+	instance_type = str(instance_type).replace("{","").replace("}", "")
+	return(instance_type)
 
 
 # Get Active Reserved Instance Reservation details
 
 def get_reserved_instance_details(session,aws_region):
+	print("Getting Reserved Instance Details")
 	ec2_client = session.client('ec2',region_name=aws_region)
 	reservedInstanceDetails = {}
 	response = ec2_client.describe_reserved_instances(
@@ -78,58 +78,129 @@ def get_reserved_instance_details(session,aws_region):
 	reservedInstanceList = []
 	count=1
 	for i in range(len(response['ReservedInstances'])):
-		reservationNumber = "Reservation-{}".format(count)
-		reservedInstanceList.append({reservationNumber: {"InstanceType":response['ReservedInstances'][i]['InstanceType'], \
-            "InstanceCount":response['ReservedInstances'][i]['InstanceCount'], \
-            "OfferingType":response['ReservedInstances'][i]['OfferingType'], \
-            "OfferingClass":response['ReservedInstances'][i]['OfferingClass'], \
-            "EndingOn":response['ReservedInstances'][i]['End'].strftime("%m/%d/%Y, %H:%M:%S")
-	    }})
+		reservedInstanceList.append( {"InstanceType":response['ReservedInstances'][i]['InstanceType'], \
+			"InstanceCount":response['ReservedInstances'][i]['InstanceCount'], \
+			"OfferingType":response['ReservedInstances'][i]['OfferingType'], \
+			"OfferingClass":response['ReservedInstances'][i]['OfferingClass'], \
+			"EndingOn":response['ReservedInstances'][i]['End'].strftime("%m/%d/%Y, %H:%M:%S")
+		})
 
 		count = count + 1
 
 	return(reservedInstanceList)
 
+
+# Write CSV Header
+def write_csv_header(csv_file,csv_columns):
+	print("Writing CSV Header")
+	with open(csv_file, 'w') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+		writer.writeheader()
+
+# Function to write CSV file
+def write_csv(csv_file,csv_columns,dict_data):
+	print("Writing CSV data")
+	with open(csv_file, 'a') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+		writer.writerow(dict_data)
+
 # If AWS Region is defined, check instances for that region
 
-if aws_regions:
-	for region in aws_regions:
-		print("======================= Instance Details =======================")
-		instanceDetails = get_instance_count_per_type(session,region)
-		if instanceDetails:
-			print("{}:{}".format(region,instanceDetails))
-		else:
-			print("{}: No Instances running in region".format(region))
+def getDetails(session,profile,aws_regions=None):
+	# If aws_regions are defined
+	if aws_regions:
+		csv_instances_filenames = []
+		csv_reserved_instances_filenames = []
+		for region in aws_regions:
+			print("\n======================================\nProfile: {} |||| Region: {}\n".format(profile,region))
+			csv_instances_filenames.append("/tmp/instances-{}-{}.csv".format(profile,region))
+			csv_reserved_instances_filenames.append("/tmp/reserved-instances-{}-{}.csv".format(profile,region))
+			os.system('touch /tmp/instances-{}-{}.csv'.format(profile,region))
+			os.system('touch /tmp/reserved-instances-{}-{}.csv'.format(profile,region))
 
-		print("======================= Reserved Instance Details =======================")
-		reservedInstances = get_reserved_instance_details(session,region)
-		if reservedInstances:
-			for reservation in reservedInstances:
-				print("{}:{}".format(region,reservation))
-		else:
-			print("{}: No Reserved Instances in region".format(region))
+			# Instances
+			instanceDetails = get_instance_count_per_type(session,region)
+			if instanceDetails:
+				data = "{{'region':'{}',{}}}".format(region,instanceDetails)
+				# Converted data to dict format
+				data = eval(data)
+				csv_file = "/tmp/instances-{}-{}.csv".format(profile,region)
+				csv_columns = data.keys()
+				write_csv_header(csv_file,csv_columns)
+				write_csv(csv_file,csv_columns,data)
+			else:
+				print("No Instances found in {} and region {}".format(profile,region))
+				for file in csv_instances_filenames:
+					os.remove(file)
+					csv_instances_filenames.remove(file)
 
-# If No Region is defined, it will check in all EC2 regions
-else:
-	ec2_client = session.client('ec2',region_name=session.region_name)
-	all_regions = ec2_client.describe_regions()
-	for record in all_regions['Regions']:
-		print("======================= Instance Details =======================")
-		instanceDetails = get_instance_count_per_type(session,record['RegionName'])
-		if instanceDetails:
-			print("{}:{}".format(record['RegionName'],instanceDetails))
-		else:
-			print("{}: No Instances running in region".format(record['RegionName']))
+			# Reserved Instances
+			reservedInstances = get_reserved_instance_details(session,region)
+			if reservedInstances:
+				csv_columns = ["region","InstanceType","InstanceCount","OfferingType","OfferingClass","EndingOn"]
+				csv_file = "/tmp/reserved-instances-{}-{}.csv".format(profile,region)
+				write_csv_header(csv_file,csv_columns)
+				for reservation in reservedInstances:
+					reservation = str(reservation).replace("{","").replace("}", "")
+					data = "{{'region':'{}',{}}}".format(region,reservation)
+					data = eval(data)
+					write_csv(csv_file,csv_columns,data)
+			else:
+				print("No Reserved Instances found in {} and region {}".format(profile,region))
+				for file in csv_reserved_instances_filenames:
+					os.remove(file)
+					csv_reserved_instances_filenames.remove(file)
 
-		print("======================= Reserved Instance Details =======================")
-		reservedInstances = get_reserved_instance_details(session,record['RegionName'])
-		if reservedInstances:
-			for reservation in reservedInstances:
-				print("{}:{}".format(record['RegionName'],reservation))
-		else:
-			print("{}: No Reserved Instances in region".format(record['RegionName']))
+			# Remove CSV files in single and size is 0
+			if csv_instances_filenames:
+				for file in csv_instances_filenames:
+					if os.stat(file).st_size == 0:
+						os.remove(file)
+						csv_instances_filenames.remove(file)
+	
+				# Create a Single Instance CSV file of instance details
+				if len(csv_instances_filenames) > 1:
+					for i in range(csv_instances_filenames[1:]):
+						os.system('cat {} >> {}'.format(csv_instances_filenames[i+1],csv_instances_filenames[0]))
+						os.remove(csv_instances_filenames[i+1])
+					os.rename(csv_instances_filenames[0],"/tmp/instance-details-{}.csv".format(profile))
+					print("------------------------------------")
+					print("Get Instance details for {} in --> /tmp/instance-details-{}.csv".format(profile,profile))
+				elif len(csv_instances_filenames) == 1:
+					os.rename(csv_instances_filenames[0],"/tmp/instance-details-{}.csv".format(profile))
+					print("------------------------------------")
+					print("Get Instance details for {} in --> /tmp/instance-details-{}.csv".format(profile,profile))
+
+			if csv_reserved_instances_filenames:
+				for file in csv_reserved_instances_filenames:
+					if os.stat(file).st_size == 0:
+							print("Removing File --> {}".format(file))
+							os.remove(file)
+							csv_reserved_instances_filenames.remove(file)
+	
+				# Create a Single Instance CSV file of Reserved instance details
+				if len(csv_reserved_instances_filenames) > 1:
+					for i in range(csv_reserved_instances_filenames[1:]):
+						os.system('cat {} >> {}'.format(csv_reserved_instances_filenames[i+1],csv_reserved_instances_filenames[0]))
+						os.remove(csv_reserved_instances_filenames[i+1])
+					os.rename(csv_reserved_instances_filenames[0],"/tmp/reserved-instance-details-{}.csv".format(profile))
+					print("------------------------------------")
+					print("Get Reserved Instance details for {} in --> /tmp/reserved-instance-details-{}.csv".format(profile,profile))
+				elif len(csv_reserved_instances_filenames) == 1:
+					os.rename(csv_reserved_instances_filenames[0],"/tmp/reserved-instance-details-{}.csv".format(profile))
+					print("------------------------------------")
+					print("Get Reserved Instance details for {} in --> /tmp/reserved-instance-details-{}.csv".format(profile,profile))
+
+			# Empting Lists
+			csv_instances_filenames = []
+			csv_reserved_instances_filenames = []
+
+	# If No Region is defined, it will check in all EC2 regions
+	else:
+		print('Error:Please Specify Region')
 
 
-
-
-
+# Actual execution
+for profile in profile_names:
+	session = boto3.Session(profile_name=profile)
+	getDetails(session,profile,aws_regions)
